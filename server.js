bfdfdcceae44c6162a5b99a485fed59e0a63f771
Dillon H <dillonrhamilton@gmail.com>
2015-12-02 23:59:29 -0500

    routing

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
+/**
+ * Module dependencies.
+ */
+
+var render = require('./lib/render');
+var logger = require('koa-logger');
+var route = require('koa-route');
+var parse = require('co-body');
 var koa = require('koa');
 var app = koa();
 
-app.use(function *(){
-    this.body = 'hello';
-});
+// "database"
+
+var posts = [];
+
+// middleware
+
+app.use(logger());
+
+// route middleware
+app.use(route.get('/', init));
+
+function *init() {
+  this.body = yield render('app');
+}
+
+// listen
 
 app.listen(3000);
+console.log('listening on port 3000');
