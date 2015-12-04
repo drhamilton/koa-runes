31eef2982ed00402c3c63bd16685a92b20d60b52
Dillon H <dillonrhamilton@gmail.com>
2015-12-03 20:06:59 -0500

    procco

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 /**
  * Module dependencies.
  */
-
 var render = require('./lib/render');
 var logger = require('koa-logger');
 var route = require('koa-route');
@@ -10,15 +9,11 @@ var koa = require('koa');
 var app = koa();
 
 var config = {};
-config.NODE_ENV = process.env.NODE_ENV || 'development';
-config.PORT = Number.parseInt(process.env.PORT, 10) || 3000;
 
 // "database"
-
 var posts = [];
 
 // middleware
-
 app.use(logger());
 
 // route middleware
@@ -29,6 +24,6 @@ function *init() {
 }
 
 // listen
-
-app.listen(config.PORT);
-console.log('listening on port config.PORT');
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Listening to %s', port);
